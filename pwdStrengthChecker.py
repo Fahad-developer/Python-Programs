30eede43804928f300c9008fd6f59b8dcc72c4c3
Fahad-developer <zareenaa137@gmail.com>
2023-01-17 23:14:34 -0800

    add comments to the start of the file

--- pwdStrengthChecker.py
+++ pwdStrengthChecker.py
@@ -1,3 +1,9 @@
+"""
+This program contains a function check_password which takes
+a single argument and tells if the password is strong or not
+
+"""
+
 def check_password(password):
     # Check that the password is at least 8 character long.
     if len(password) < 8:
@@ -13,7 +19,7 @@ def check_password(password):
         if not has_digit:
             return "Kindly use at least one digit in your password."
 
-        # Checl that the password contains at least one uppercase letter.
+        # Check that the password contains at least one uppercase letter.
         has_uppercase = False
         for char in password:
             if char.isupper():
@@ -33,7 +39,6 @@ def check_password(password):
 
             # If all checks pass, the password is considered valid
             return True
-
 
 password = input("Enter your password: ")
 print(check_password(password))
